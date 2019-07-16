04769df749aac53decad0eb284144cf663285614
Dimitar Stanev <jimstanev@gmail.com>
2019-07-16 17:24:57 +0300

    incorporate lrs (TODO testing)

--- source_code_opensim_v4.0/src/feasible_muscle_force_analysis/FeasibleMuscleForceAnalysis.cpp
+++ source_code_opensim_v4.0/src/feasible_muscle_force_analysis/FeasibleMuscleForceAnalysis.cpp
@@ -2,10 +2,15 @@
 #include <cassert>
 #include <OpenSim/Simulation/Model/Model.h>
 #include <OpenSim/Simulation/Model/Muscle.h>
+extern "C" {
+#include "lrslib.h"
+};
 
 using namespace std;
 using namespace OpenSim;
 using namespace SimTK;
+
+/******************************************************************************/
 
 Matrix calculateMomentArm(const State& s, const Model& model) {
     auto& coordinateSet = model.getCoordinateSet();
@@ -37,7 +42,7 @@ Matrix calculateNullSpace(const Matrix& A, double atol=1e-13, double rtol=0) {
     auto tol = max(atol, rtol * s[0]);
     int nz = 0;
     for (auto v : s) {
-	if (v >= tol) nz++;
+        if (v >= tol) nz++;
     }
     Matrix N = VT(nz, 0, VT.nrow() - nz, VT.ncol()).transpose();
     return N;
@@ -45,15 +50,15 @@ Matrix calculateNullSpace(const Matrix& A, double atol=1e-13, double rtol=0) {
 
 void testNullSpace() {
     Real a[24] = {0.03197292, 0.87063039, 0.13821516, 0.13098836, 0.75546764, 0.42618434,
-		  0.87926417, 0.09365083, 0.10514934, 0.57219935, 0.59429898, 0.27111457,
-		  0.48186018, 0.73522382, 0.05211834, 0.11899367, 0.08551981, 0.50789553,
-		  0.74345284, 0.39507762, 0.64844182, 0.92448094, 0.81439658, 0.66590865};
+                  0.87926417, 0.09365083, 0.10514934, 0.57219935, 0.59429898, 0.27111457,
+                  0.48186018, 0.73522382, 0.05211834, 0.11899367, 0.08551981, 0.50789553,
+                  0.74345284, 0.39507762, 0.64844182, 0.92448094, 0.81439658, 0.66590865};
     Real n[12] = {0.27668194, -0.19129641,
-		 -0.21901866, -0.39516696,
-		  0.49314911, -0.41430876,
-		 -0.75577206, -0.0449566 ,
-		  0.19822795,  0.10174348,
-		  0.14763525,  0.78944549};
+                  -0.21901866, -0.39516696,
+                  0.49314911, -0.41430876,
+                  -0.75577206, -0.0449566 ,
+                  0.19822795,  0.10174348,
+                  0.14763525,  0.78944549};
     Matrix A(4, 6, a);
     Matrix Nr(6, 2, n);
     auto N = calculateNullSpace(A);
@@ -61,24 +66,24 @@ void testNullSpace() {
     // cout << A << endl;
     // cout << N << endl;
     // cout << Nr << endl;
-    
+
     // check if result is correct
     for (int i = 0; i < Nr.nrow(); i++) {
-	assert((N[i] - Nr[i]).norm() < 0.0001);
+        assert((N[i] - Nr[i]).norm() < 0.0001);
     }
 
     // check if this is a null space matrix
     auto AN  = A * N;
     for (int i = 0; i < AN.nrow(); i++) {
-	assert(AN[i].norm() < 0.0001);
+        assert(AN[i].norm() < 0.0001);
     }
 }
 
 void constructLinearMuscleInequality(const Matrix& NR,
-				     const Vector& fmPar,
-				     const Vector& fmMax,
-				     Matrix& Z,
-				     Vector& b) {
+                                     const Vector& fmPar,
+                                     const Vector& fmMax,
+                                     Matrix& Z,
+                                     Vector& b) {
     Z = Matrix(2 * NR.nrow(), NR.ncol());
     Z(0, 0, NR.nrow(), NR.ncol()) = -1.0 * NR;
     Z(NR.nrow(), 0, NR.nrow(), NR.ncol()) = NR;
@@ -90,12 +95,85 @@ void constructLinearMuscleInequality(const Matrix& NR,
     // assert((b(0, m) - fmPar).norm() < 0.0001);
     // assert((b(m, m) - (fmMax - fmPar)).norm() < 0.0001);
     // for (int i = 0; i < Z.nrow() / 2; i++) {
-    // 	assert((Z[i] + NR[i]).norm() < 0.0001);
+    //  assert((Z[i] + NR[i]).norm() < 0.0001);
     // }
     // for (int i = Z.nrow() / 2; i < Z.nrow(); i++) {
-    // 	assert((Z[i] - NR[i]).norm() < 0.0001);
+    //  assert((Z[i] - NR[i]).norm() < 0.0001);
     // }
 }
+
+void vertexEnumeration(const Matrix& A, const Vector& b) {
+    // variables
+    lrs_dic* P;
+    lrs_dat* Q;
+    lrs_mp_vector output;
+    lrs_mp_matrix Lin;   
+    long i;
+    long col;
+
+    // initialize
+    if (!lrs_init((char*) "lrs")) {
+	throw OpenSim::Exception("could not initialize lrs library");
+    }
+        
+    // initialize Q data
+    Q = lrs_alloc_dat("lrs_data");
+    if (Q == NULL) {
+	throw OpenSim::Exception("could not allocate Q data");
+    }
+    Q->m = A.nrow();
+    Q->n = A.ncol() + 1;
+
+    // initialize P dictionary
+    P = lrs_alloc_dic(Q); 
+    if (P == NULL) {
+	throw OpenSim::Exception("could not allocate P dictionary");
+    }
+
+    // populate P and Q from A and b
+    lrs_mp_vector num, den;
+    num = lrs_alloc_mp_vector(Q->n);
+    den = lrs_alloc_mp_vector(Q->n);
+    for (int i = 1; i <= Q->m; i++){
+	// TODO populate num, den
+	lrs_set_row_mp(P, Q, i, num, den, GE);
+    }
+
+    // pivot to a starting dictionary
+    output = lrs_alloc_mp_vector(Q->n);
+    if (!lrs_getfirstbasis(&P, Q, &Lin, false)) {
+	// print non redundant coefficients
+        for (col = 0L; col < Q->nredundcol; col++) 
+            lrs_printoutput(Q, Lin[col]);
+
+        do {
+            for (col = 0; col <= P->d; col++)
+                if (lrs_getsolution(P, Q, output, col))
+                    lrs_printoutput(Q, output);
+        } while (lrs_getnextbasis(&P, Q, FALSE));
+
+        lrs_printtotals(P, Q);
+
+	// free space
+        lrs_clear_mp_vector(output, Q->n);
+	lrs_clear_mp_vector(num, Q->n);
+        lrs_clear_mp_vector(den, Q->n);
+        lrs_free_dic(P, Q);
+        lrs_free_dat(Q);
+	lrs_close((char*) "lrs");
+    } else {
+	// free space
+	lrs_clear_mp_vector(output, Q->n);
+	lrs_clear_mp_vector(num, Q->n);
+        lrs_clear_mp_vector(den, Q->n);
+        lrs_free_dic(P, Q);
+        lrs_free_dat(Q);
+	lrs_close((char*) "lrs");
+	throw OpenSim::Exception("empty set");
+    }
+}
+
+/******************************************************************************/
 
 FeasibleMuscleForceAnalysis::FeasibleMuscleForceAnalysis() : Analysis() {
     setNull();
@@ -127,8 +205,8 @@ int FeasibleMuscleForceAnalysis::end(const SimTK::State & s) {
 
 int FeasibleMuscleForceAnalysis::printResults(const string& aBaseName,
                                               const string& aDir,
-					      double aDT,
-					      const string& aExtension) {
+                                              double aDT,
+                                              const string& aExtension) {
     return 0;
 }
 
@@ -146,17 +224,17 @@ int FeasibleMuscleForceAnalysis::record(const State& s) {
     auto R = calculateMomentArm(s, *_model);
     auto NR = calculateNullSpace(R);
     Vector fmPar(m);
-    soStorage->getDataAtTime(s.getTime(), m, fmPar);   
+    soStorage->getDataAtTime(s.getTime(), m, fmPar);
 
     // formulate inequlaties
     Matrix Z;
     Vector b;
     constructLinearMuscleInequality(NR, fmPar, fmMax, Z, b);
 
-    
+
 
     exit(0);
-    
+
     // testNullSpace();
     // exit(0);
 
